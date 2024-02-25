81eba7febf35fa75f062d3687921ac4890525b85
Baptiste Audugé <baptiste.auduge@gmail.com>
2024-02-25 20:25:27 +0100

    fix: added justify text en align things

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import 'styles/components/ExperienceItem.scss'
-import { Tag } from './Tag'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import 'styles/components/ExperienceItem.scss';
+import { Tag } from './Tag';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 interface ExperienceItemProps {
-  beginYear: string,
-  endYear: string,
-  keyAchievements?: Array<string | { achievement: string, subElements: Array<string> }>,
-  responsibilities?: Array<string>,
-  additionalInformations?: Array<string>,
-  tags: Array<string>,
-  status: "default" | "lowOpacity",
-  title: string,
-  link: string,
+  beginYear: string;
+  endYear: string;
+  keyAchievements?: Array<
+    string | { achievement: string; subElements: Array<string> }
+  >;
+  responsibilities?: Array<string>;
+  additionalInformations?: Array<string>;
+  tags: Array<string>;
+  status: 'default' | 'lowOpacity';
+  title: string;
+  link: string;
 }
 
 export const ExperienceItem: React.FC<ExperienceItemProps> = ({
@@ -25,78 +27,91 @@ export const ExperienceItem: React.FC<ExperienceItemProps> = ({
   tags,
   status,
   title,
-  link
+  link,
 }) => {
   return (
-    <div className={`experience-item ${status}`} onClick={
-      () => {
-        window.open(link, "_blank");
-      }
-    }>
+    <div
+      className={`experience-item ${status}`}
+      onClick={() => {
+        window.open(link, '_blank');
+      }}
+    >
       <div className="date-column">
-        <p>{beginYear} - {endYear}</p>
+        <p>
+          {beginYear} - {endYear}
+        </p>
       </div>
       <div className="content-column">
-        <p className='date-phone'>{beginYear} - {endYear}</p>
-        <h3>{title} <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></h3>
-        {keyAchievements?.length ? <p className='key-achievements-title'><b>Key Achievements :</b></p> : null}
+        <p className="date">
+          {beginYear} - {endYear}
+        </p>
+        <h3>
+          {title} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+        </h3>
+        {keyAchievements?.length ? (
+          <p className="key-achievements-title">
+            <b>Key Achievements :</b>
+          </p>
+        ) : null}
         <ul>
-          {
-            keyAchievements?.map((achievement, index) => {
-              return (
-                <li key={index}>
-                  {
-                    typeof achievement === "string" ? <p>{achievement}</p> : (
-                      <div>
-                        <p>{achievement.achievement}</p>
-                        <ul>
-                          {
-                            achievement.subElements.map((subElement, index) => {
-                              return (
-                                <li key={index}><p>{subElement}</p></li>
-                              )
-                            })
-                          }
-                        </ul>
-                      </div>
-                    )
-                  }
-                </li>
-              )
-            })
-          }
+          {keyAchievements?.map((achievement, index) => {
+            return (
+              <li key={index}>
+                {typeof achievement === 'string' ? (
+                  <p>{achievement}</p>
+                ) : (
+                  <div>
+                    <p>{achievement.achievement}</p>
+                    <ul>
+                      {achievement.subElements.map((subElement, index) => {
+                        return (
+                          <li key={index}>
+                            <p>{subElement}</p>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                )}
+              </li>
+            );
+          })}
         </ul>
-        {responsibilities?.length ? <p className='responsibilities-title'><b>Responsibilities :</b></p> : null}
+        {responsibilities?.length ? (
+          <p className="responsibilities-title">
+            <b>Responsibilities :</b>
+          </p>
+        ) : null}
         <ul>
-          {
-            responsibilities?.map((responsibility, index) => {
-              return (
-                <li key={index}><p>{responsibility}</p></li>
-              )
-            })
-          }
+          {responsibilities?.map((responsibility, index) => {
+            return (
+              <li key={index}>
+                <p>{responsibility}</p>
+              </li>
+            );
+          })}
         </ul>
 
-        {additionalInformations?.length ? <p className='additional-informations-title'><b>Additional Informations :</b></p> : null}
+        {additionalInformations?.length ? (
+          <p className="additional-informations-title">
+            <b>Additional Informations :</b>
+          </p>
+        ) : null}
         <ul>
-          {
-            additionalInformations?.map((additionalInformation, index) => {
-              return (
-                <li key={index}><p>{additionalInformation}</p></li>
-              )
-            })
-          }
+          {additionalInformations?.map((additionalInformation, index) => {
+            return (
+              <li key={index}>
+                <p>{additionalInformation}</p>
+              </li>
+            );
+          })}
         </ul>
         <div className="tags">
-          {
-            tags.map((tag, index) => {
-              return (
-                <Tag key={index}>{tag}</Tag>
-              )
-            })
-          }
+          {tags.map((tag, index) => {
+            return <Tag key={index}>{tag}</Tag>;
+          })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,48 +1,45 @@
-import React from 'react'
-import 'styles/components/ProjectItem.scss'
-import { Tag } from './Tag'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import 'styles/components/ProjectItem.scss';
+import { Tag } from './Tag';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 interface ProjectItemProps {
-  contentLeft: React.ReactNode,
-  children: React.ReactNode,
-  tags: Array<string>,
-  status: "default" | "lowOpacity",
-  title: string,
-  link: string,
+  date: React.ReactNode;
+  children: React.ReactNode;
+  tags: Array<string>;
+  status: 'default' | 'lowOpacity';
+  title: string;
+  link: string;
 }
 
 export const ProjectItem: React.FC<ProjectItemProps> = ({
-  contentLeft,
+  date,
   children,
   tags,
   status,
   title,
-  link
+  link,
 }) => {
   return (
-    <div className={`project-item ${status}`} onClick={
-      () => {
-        window.open(link, "_blank");
-      }
-    }>
-      <div className="first-column">
-        {contentLeft}
-      </div>
+    <div
+      className={`project-item ${status}`}
+      onClick={() => {
+        window.open(link, '_blank');
+      }}
+    >
       <div className="content-column">
-        <h3>{title} <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></h3>
+        <p className="date">{date}</p>
+        <h3>
+          {title} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+        </h3>
         {children}
         <div className="tags">
-          {
-            tags.map((tag, index) => {
-              return (
-                <Tag key={index}>{tag}</Tag>
-              )
-            })
-          }
+          {tags.map((tag, index) => {
+            return <Tag key={index}>{tag}</Tag>;
+          })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
